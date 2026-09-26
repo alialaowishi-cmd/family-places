82bf7a28b86aad0a32be6e6401a87693f2cb07d1
alialaowishi-cmd <ali.al.aowishi@gmail.com>
2026-09-26 20:13:22 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-storage.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAppr7PHEnnsTYjL3LB0kkMQTnc4qgNR_4",
@@ -9,12 +9,12 @@ const firebaseConfig = {
   projectId: "ali-alaowishi",
   storageBucket: "ali-alaowishi.firebasestorage.app",
   messagingSenderId: "536583940962",
-  appId: "1:536585096258:web:4d4e8c037e4b5d6a7f8e90"
+  appId: "1:536583940962:web:YOUR_APP_ID_HERE"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const auth = getAuth(app);
 const storage = getStorage(app);
+const auth = getAuth(app);
 
-export { app, db, auth, storage };
+export { db, storage, auth };
